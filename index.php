4fba812ac8d5f55c13057b78fa8e8a1511648e6e
AridCommunicationSystems <97019988+AridCommunicationSystems@users.noreply.github.com>
2022-01-03 08:47:51 +0000

    Add files via upload

--- index.php
+++ index.php
@@ -8,46 +8,47 @@
 <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
 
 </head>
-
+<body>
+<html>
 		<div>
         	<div >
-            	<label class="form-label">ID</label><input name="id" id="id" class="form-input" value = "1256" readonly>
+            	<label class="form-label">ID</label><input name="id" id="id" class="form-input" value = "1256" >
             </div>
 
             <div >
-            <label class="form-label">Name</label><input type="text" name="name" id="name" class="myData" value = "Your Name 1st" readonly>
+            <label class="form-label">Name</label><input type="text" name="name" id="name" class="myData" value = "Your Name 1st" >
             </div>
             
             <div >
-            <label class="form-label">Name2</label><input type="text" name="name2" id="name2" class="myData" value = "Your Name  2nd" readonly>
+            <label class="form-label">Name2</label><input type="text" name="name2" id="name2" class="myData" value = "Your Name  2nd" >
             </div>
             <div >
-            <label class="form-label">Name3</label><input type="text" name="name3" id="name3" class="myData" value = "Your Name  3rd" readonly>
+            <label class="form-label">Name3</label><input type="text" name="name3" id="name3" class="myData" value = "Your Name  3rd" >
             </div>
             
             <div >
-            <label class="form-label">Strapline</label><input type="text" name="strapline" id="strapline" class="myData" value = "Strapline here" readonly>
+            <label class="form-label">Strapline</label><input type="text" name="strapline" id="strapline" class="myData" value = "Strapline here" >
             </div>
 
             <div >
-            <label class="form-label">Address</label><input type="text" name="add1" id="add1" class="myData" value = "Address - Street"readonly >
+            <label class="form-label">Address</label><input type="text" name="add1" id="add1" class="myData" value = "Address - Street" >
             </div>
 
             <div >
-            <label class="form-label">Town</label><input type="text" name="town" id="town" class="myData" value = "Address - Town"readonly >            
+            <label class="form-label">Town</label><input type="text" name="town" id="town" class="myData" value = "Address - Town" >            
             </div>
 
             <div >
-            <label class="form-label">Town</label><input type="text" name="county" id="county" class="myData" value = "Address - County" readonly>            
+            <label class="form-label">Town</label><input type="text" name="county" id="county" class="myData" value = "Address - County" >            
 			</div>
             <div >
-            <label class="form-label">Postcode</label><input type="text" name="postcode" id="postcode" class="myData" value = "Address - Postcode" readonly>            
+            <label class="form-label">Postcode</label><input type="text" name="postcode" id="postcode" class="myData" value = "Address - Postcode" >            
 			</div>
             <div >
-            <label class="form-label">Telephone</label><input type="text" name="telephone" id="telephone" class="myData" value = "Your Telephone" readonly>            
+            <label class="form-label">Telephone</label><input type="text" name="telephone" id="telephone" class="myData" value = "Your Telephone" >            
 			</div>
             <div >
-            <label class="form-label">Email</label><input name="email" id="email" class="myData" type="email"value = "your@email.address" readonly>            
+            <label class="form-label">Email</label><input type="email" name="email" id="email" class="myData" value = "your@email.address" >            
 			</div>
 			<div>
         	<button  id="showData"> Show data </button> 
@@ -57,9 +58,6 @@
 
 		<div id="showData"></div>
 
-
-
-<body>
 </body>
 </html>
 <script>
